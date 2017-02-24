93c34208800dc0d26e658a9adac350be45afdcde
SlavikBstu <slava-ilin-96@mail.ru>
2017-02-25 02:34:56 +0300

    Login with attempts

--- Login/app/src/main/java/com/example/apache/login/LoginActivity.java
+++ Login/app/src/main/java/com/example/apache/login/LoginActivity.java
@@ -14,6 +14,8 @@ public class LoginActivity extends AppCompatActivity {
     EditText edPassword;
     Button Login;
 
+    public static int attempts;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,17 +25,59 @@ public class LoginActivity extends AppCompatActivity {
         edPassword = (EditText)findViewById(R.id.editPassword);
 
         Login = (Button)findViewById(R.id.buttonLogin);
+
+        attempts = 5;
     }
 
-    public void login(View view){
-        if(edUsername.getText().toString().equals("admin") && edPassword.getText().toString().equals("admin")){
+    public void login(View view) {
+        if (edUsername.getText().toString().equals("admin") && edPassword.getText().toString().equals("admin")) {
             Intent intent = new Intent(LoginActivity.this, WelcomeActivity.class);
             startActivity(intent);
         }
-        else{
-            Toast.makeText(getApplicationContext(), "Wrong password! Repeat, please", Toast.LENGTH_SHORT).show();
-            Login.setEnabled(false);
+        else {
+            if (editdist("admin", edPassword.getText().toString()) == 1) {
+                attempts--;
+                Toast.makeText(getApplicationContext(), "You have " + attempts + " attempts! Repeat, please", Toast.LENGTH_SHORT).show();
+                if (attempts == 0) {
+                    this.finish();
+                }
+            }
+            if (editdist("admin", edPassword.getText().toString()) > 1) {
+                int second_attempts = attempts - 3;
+                Toast.makeText(getApplicationContext(), "You have " + second_attempts + " attempts! Repeat, please", Toast.LENGTH_SHORT).show();
+                attempts--;
+                if (attempts <= 2) {
+                    this.finish();
+                }
+            }
         }
+    }
+
+    int editdist(String S1, String S2) {
+        int m = S1.length(), n = S2.length();
+        int[] D1;
+        int[] D2 = new int[n + 1];
+
+        for(int i = 0; i <= n; i ++)
+            D2[i] = i;
+
+        for(int i = 1; i <= m; i ++) {
+            D1 = D2;
+            D2 = new int[n + 1];
+            for(int j = 0; j <= n; j ++) {
+                if(j == 0) D2[j] = i;
+                else {
+                    int cost = (S1.charAt(i - 1) != S2.charAt(j - 1)) ? 1 : 0;
+                    if(D2[j - 1] < D1[j] && D2[j - 1] < D1[j - 1] + cost)
+                        D2[j] = D2[j - 1] + 1;
+                    else if(D1[j] < D1[j - 1] + cost)
+                        D2[j] = D1[j] + 1;
+                    else
+                        D2[j] = D1[j - 1] + cost;
+                }
+            }
+        }
+        return D2[n];
     }
 
 }
